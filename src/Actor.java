63482144d1176487779602d364d1f25090aa6dbb
unknown <BT_4N2_24@bktech.local>
2023-02-07 12:33:41 -0500

    Initial commit

---
+++ src/Actor.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+public class Actor {
+    private String name;
+    private ArrayList<SimpleMovie> moviesActorIsIn;
+    public Actor(){
+
+    }
+    public boolean compareActor(Actor otherActor){
+        int smallerList = Math.min(this.moviesActorIsIn.size(), otherActor.moviesActorIsIn.size());
+        for(int i = 0; i < smallerList; i++){
+
+        }
+        return false;
+    }
+
+    public
+
+}
